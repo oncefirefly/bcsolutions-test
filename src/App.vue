c124478429f5d774f2e75b0dadab7a57392a4f2c
oncefirefly <92741839+oncefirefly@users.noreply.github.com>
2022-05-16 20:07:18 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <header class="header">
     <div class="header__content content">
       <nav>
-        <router-link to="/">Converter</router-link>
+        <router-link to="/bssolutions-test/converter">Converter</router-link>
         <router-link to="/bcsolutions-test/portfolio">Portfolio</router-link>
       </nav>
     </div>
